5fa4221026f4ec52f853a6f7d8bd906e5cf9f99d
Jonathan Davila <jonathan@davila.io>
2018-09-14 16:10:49 -0400

    updates to docs for ls and some better safety check on 0

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -15,6 +15,7 @@ class LsCommand extends Command {
     const { flags, args } = this.parse(LsCommand)
     const { benchmarkId } = args
     const type = benchmarkId ? 'rule' : 'benchmark'
+    debug('type', type)
     const { cats, json } = flags
     let severities = cats
     if (cats === 'all') {
@@ -31,9 +32,11 @@ class LsCommand extends Command {
       data = res.data
     } else {
       const params = { dataDir, severities }
+      debug('is benchmark id a number', !isNaN(benchmarkId), benchmarkId)
       isNaN(benchmarkId)
         ? params.benchmarkTitle = benchmarkId
         : params.benchmarkIndex = Number(benchmarkId)
+      debug('getrules params', params)
 
       res = await getRules(params)
       if (res.err) {
@@ -57,33 +60,71 @@ When supplied without arguments it returns a list of all available benchmarks.
 Example output
 
 $ stig ls
-╔═════╤═════════════════════════════════════════════════════════╤══════╤══════╤════════════╗
-║ ID  │ Title                                                   │ Ver. │ Rel. │ Date       ║
-╟─────┼─────────────────────────────────────────────────────────┼──────┼──────┼────────────╢
-║ 0   │ A10 Networks Application Delivery Controller (ADC) ALG  │ 1    │ 1    │ 4/27/2016  ║
-╟─────┼─────────────────────────────────────────────────────────┼──────┼──────┼────────────╢
-║ 1   │ A10 Networks Application Delivery Controller (ADC) NDM  │ 1    │ 1    │ 4/27/2016  ║
-╟─────┼─────────────────────────────────────────────────────────┼──────┼──────┼────────────╢
 
-And then if you want to list the rules inside of benchmarks supply an ID number
+ ID   Title                                                    Ver.  Rel.  Date
+
+ 1    A10 Networks ADC ALG                                     1     1     Apr 15, 2016
+
+ 2    A10 Networks ADC NDM                                     1     1     Apr 15, 2016
+
+
+And then if you want to list the rules inside of benchmarks supply the ID number OR the title itself
 
 Example output
-$ stig ls 0
-╔════╤═══════════════════════════════════════════════╤═════════╤═════════════════╤══════════╗
-║ ID │ Title                                         │ Vuln ID │ Rule ID         │ Severity ║
-╟────┼───────────────────────────────────────────────┼─────────┼─────────────────┼──────────╢
-║ 0  │ The A10 Networks ADC, when used for TLS       │ V-67957 │ SV-82447r1_rule │ medium   ║
-║    │ encryption anddecryption, must be configured  │         │                 │          ║
-║    │ to comply with the required TLSsettings in    │         │                 │          ║
-║    │ NIST SP 800-52.                               │         │                 │          ║
-╟────┼───────────────────────────────────────────────┼─────────┼─────────────────┼──────────╢
-║ 1  │ The A10 Networks ADC, when used to load       │ V-67959 │ SV-82449r1_rule │ low      ║
-║    │ balance webapplications, must enable external │         │                 │
+$ stig ls 1
+ STIG ID  Rule ID                                                  Title    Severity
+
+ medium   The A10 Networks ADC must generate an alert to, at a     V-68105  SV-82595r1_rule
+          minimum, the ISSO and ISSM when threats identified by
+          authoritative sources (e.g., IAVMs or CTOs) are
+          detected.
+
+ high     The A10 Networks ADC must be a FIPS-compliant version.   V-68029  SV-82519r1_rule
+
+ medium   The A10 Networks ADC must protect against TCP SYN        V-68027  SV-82517r1_rule
+          floods by using TCP SYN Cookies.
+
+When supplying the title make sure to wrap the title in quotes
+Example output
+$ stig ls 'Windows 10'
+ STIG ID  Rule ID                                                  Title    Severity
+
+ high     Administrative accounts must not be used with            V-78129  SV-92835r1_rule
+          applications that access the Internet, such as web
+          browsers, or with potential Internet sources, such as
+          email.
+
+ medium   Exploit Protection mitigations in Windows 10 must be     V-77269  SV-91965r2_rule
+          configured for wordpad.exe.
+
+ medium   Exploit Protection mitigations in Windows 10 must be     V-77267  SV-91963r2_rule
+          configured for wmplayer.exe.
+
+If you want only certain severities you can pass the --cats/-c flag. By default it returns all the rules. If you do \`-c high -c\` low it will only return low and high.
+
+Example output
+$ stig ls 'Windows 10' -c high
+
+ STIG ID  Rule ID                                                  Title    Severity
+
+ high     Administrative accounts must not be used with            V-78129  SV-92835r1_rule
+          applications that access the Internet, such as web
+          browsers, or with potential Internet sources, such as
+          email.
+
+ high     Structured Exception Handling Overwrite Protection       V-68849  SV-83445r4_rule
+          (SEHOP) must be enabled.
+
+ high     Data Execution Prevention (DEP) must be configured to    V-68845  SV-83439r2_rule
+          at least OptOut.
+
 `
 
 LsCommand.examples = [
   'stig ls',
-  'stig ls 200'
+  'stig ls 200',
+  'stig ls "Windows 10"',
+  'stig ls "Windows 10" -c low -c medium'
 
 ]
 
@@ -91,7 +132,7 @@ LsCommand.args = [
   {
     name: 'benchmarkId',
     required: false,
-    description: 'OPTIONAL: List rules for a specific STIG Benchmark'
+    description: 'OPTIONAL: List rules for a specific STIG Benchmark. Supply the ID or title'
   }
 ]
 
--- src/utils/query.js
+++ src/utils/query.js
@@ -20,9 +20,10 @@ const getBenchmarks = async (dataDir) => {
 
 const getBenchmark = async ({ dataDir, title, index }) => {
   try {
-    if (title && index) {
+    debug('benchmark index', index)
+    if (title && index !== undefined) {
       throw new Error('title and index supplied to getBenchmark(), only one is allowed')
-    } else if (!title && !index) {
+    } else if (!title && index === undefined) {
       throw new Error('either title or index must be supplied when calling getBenchmark')
     }
 
@@ -41,22 +42,25 @@ const getBenchmark = async ({ dataDir, title, index }) => {
     }
   } catch (err) {
     debug('error in getBenchmark in query')
+    debug(err.message)
     return { err }
   }
 }
 
 const getRules = async ({ dataDir, benchmarkTitle, benchmarkIndex, severities }) => {
+  debug('benchmarkIndex', benchmarkIndex)
   try {
     if (benchmarkTitle && benchmarkIndex) {
       throw new Error('benchmarkTitle and benchmarkIndex supplied to getRules(), only one is allowed')
     } else if (!benchmarkTitle && !benchmarkIndex) {
-      throw new Error('either benchmarkTitle or benchmarkIndex is required when calling getRules')
+      throw new Error('either benchmarkTitle or benchmarkIndex is required when calling getRules. Just in case, 0 is not a valid index number.')
     }
 
     const { err: errDb, rulesDb } = await getDb(dataDir)
     if (!rulesDb) {
       throw new Error('Database not found, you must call init via the CLI or API before querying. See `stig init --help` for more details')
     }
+
     if (errDb) {
       throw errDb
     }
@@ -68,15 +72,21 @@ const getRules = async ({ dataDir, benchmarkTitle, benchmarkIndex, severities })
 
     debug('params', params)
     const { data } = await getBenchmark(params)
+    if (!data) {
+      throw new Error(`No benchmark found at index ${benchmarkIndex}`)
+    }
+
     const { $loki } = data
     const rParams = {
       stigIndex: $loki
     }
+
     if (severities) rParams.severity = { '$in': severities }
 
     return { data: rulesDb.find(rParams) }
   } catch (err) {
-    debug('error in getBenchmark in query')
+    debug('error in getRules in query')
+    debug(err.message)
     return { err }
   }
 }
@@ -104,7 +114,8 @@ const getRule = async ({ dataDir, stigId, ruleId, stigIndex }) => {
     const data = rulesDb.findOne(query)
     return { data }
   } catch (err) {
-    debug('error in getBenchmark in query')
+    debug('error in getRule in query')
+    debug(err.message)
     return { err }
   }
 }
