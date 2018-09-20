237e714386285b8903228f5db11eb544a1437a37
Jonathan Davila <jonathan@davila.io>
2018-09-20 16:33:49 -0400

    refactor update-sources

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,10 @@
 const {
   readFile,
-  unlinkSync,
-  existsSync,
-  mkdirSync,
-  readdir,
-  createWriteStream,
-  createReadStream
+  readdir
 } = require('fs')
 const { promisify } = require('util')
 const { join } = require('path')
-const unzipper = require('unzipper')
 const debug = require('debug')('utils:index')
-const fetch = require('node-fetch')
 const { tidy } = require('htmltidy')
 const parser = require('fast-xml-parser')
 const moment = require('moment')
@@ -23,7 +16,7 @@ const decodeToObj = async str => {
   return { data }
 }
 
-module.exports.getRuleData = async rule => {
+const getRuleData = async rule => {
   let VulnDiscussion
   try {
     const {
@@ -70,7 +63,7 @@ module.exports.getRuleData = async rule => {
   }
 }
 
-module.exports.getBenchmarkData = async xml => {
+const getBenchmarkData = async xml => {
   debug('start getBenchmarkData()')
   try {
     const {
@@ -101,7 +94,7 @@ module.exports.getBenchmarkData = async xml => {
   }
 }
 
-module.exports.getDataPaths = async () => {
+const getDataPaths = async () => {
   try {
     const rDir = promisify(readdir)
     const filesData = await rDir(join(__dirname, '../../data/benchmarks'))
@@ -141,7 +134,7 @@ const parseXmlStr = async (xmlData) => {
   return { data }
 }
 
-module.exports.getXmlData = async ({ file }) => {
+const getXmlData = async ({ file }) => {
   try {
     const rFile = promisify(readFile)
     const xmlAsString = await rFile(file)
@@ -156,120 +149,9 @@ module.exports.getXmlData = async ({ file }) => {
   }
 }
 
-module.exports.download = async ({ url, title, tmpDir }) => {
-  try {
-    const fileName = url.substring(url.lastIndexOf('/') + 1)
-    const archiveDir = join(tmpDir, 'zip_archives')
-    const archivePath = join(archiveDir, fileName)
-
-    if (!existsSync(archiveDir)) mkdirSync(archiveDir)
-
-    if (!existsSync(archivePath)) {
-      const data = await fetch(url)
-        .then((res) => res.ok ? res
-          : Promise.reject(new Error(`Initial error downloading file => ${res.error}`))
-        )
-        .then((res) => {
-          if (!res.ok) {
-            return Promise.reject(new Error({
-              reason: 'Initial error downloading file',
-              meta: {url, error: new Error(res.statusText)}
-            }))
-          }
-
-          const stream = createWriteStream(archivePath)
-          let timer
-
-          return new Promise((resolve, reject) => {
-            const errorHandler = (error) => {
-              debug('errorHandler')
-              reject(new Error(error))
-            }
-
-            res.body
-              .on('error', errorHandler)
-              .pipe(stream)
-
-            stream
-              .on('open', () => {
-                timer = setTimeout(() => {
-                  stream.close()
-                  debug('dl timeout')
-                  reject(new Error(`Timed out downloading file from ${url}`))
-                }, 10e5)
-              })
-              .on('error', errorHandler)
-              .on('finish', () => {
-                debug(`Finished downloading ${url}`)
-                resolve(archivePath)
-              })
-          }).then((archivePath) => {
-            clearTimeout(timer)
-            return archivePath
-          }, (err) => {
-            clearTimeout(timer)
-            return Promise.reject(err)
-          })
-        })
-      return { data }
-    } else {
-      return { data: archivePath }
-    }
-  } catch (err) {
-    return { err }
-  }
-}
-
-module.exports.extract = ({ archivePath, desiredFile }) => {
-  return new Promise((resolve, reject) => {
-    try {
-      const desiredFileFullPath = join(__dirname, '../../', desiredFile)
-      if (!archivePath.includes('U_Apple_OS_X_10-8')) {
-        createReadStream(archivePath)
-          .pipe(unzipper.ParseOne(/.*Manual-xccdf.xml/i))
-          .on('error', err => {
-            debug(err)
-            debug('ERROR')
-            debug(`No xccdf found in ${archivePath}`)
-            unlinkSync(desiredFileFullPath)
-            return reject(err)
-          })
-          .pipe(createWriteStream(desiredFileFullPath))
-          .on('finish', () => {
-            return resolve({ data: 'complete' })
-          })
-      } else {
-        // for some reason Apple Workstation 10.8 STIGs xccdf is zipped up
-        // inside a zip. ¯\_(ツ)_/¯
-        debug('Working on a zip inside a zip')
-        const secondZipPath = archivePath.replace('.zip', '-secondary.zip')
-        createReadStream(archivePath)
-          .pipe(unzipper.ParseOne(/.*.zip/i)) // nested ZIP archives YAY!
-          .on('error', err => {
-            debug(err)
-            debug('ERROR')
-            unlinkSync()
-            return reject(err)
-          })
-          .pipe(createWriteStream(secondZipPath))
-          .on('finish', () => {
-            createReadStream(secondZipPath)
-              .pipe(unzipper.ParseOne(/.*Manual-xccdf.xml/i))
-              .on('error', err => {
-                debug(err)
-                debug('ERROR')
-                unlinkSync()
-                return reject(err)
-              })
-              .pipe(createWriteStream(desiredFileFullPath))
-              .on('finish', () => {
-                return resolve({ data: 'complete' })
-              })
-          })
-      }
-    } catch (err) {
-      debug(err)
-      return reject(err)
-    }
-  })
+module.exports = {
+  getRuleData,
+  getDataPaths,
+  getXmlData,
+  getBenchmarkData
 }
